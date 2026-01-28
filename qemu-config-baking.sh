9edaad0399a1e300d560b9f2767748f47e615837
Jonathan <koffeinmissbrukarn@gmail.com>
2026-01-28 16:15:51 +0100

    panic

--- qemu-config-baking.sh
+++ qemu-config-baking.sh
@@ -13,16 +13,21 @@ esac
 
 # Checking if there disk1 and disk2 is already on the system, if it already is it will skip this step.
 if [ -n "$disk1" ] && [ -n "$disk2" ] && [ ! -e "$disk1" ] && [ ! -e "$disk2" ]; then
-qemu-img create -f qcow2 "$disk1" 3G
-qemu-img create -f qcow2 "$disk2" 3G
+  qemu-img create -f qcow2 "$disk1" 3G
+  qemu-img create -f qcow2 "$disk2" 3G
 fi
 
-#  echo "$disk1"
+if [ ! -f "$HOME/arch_VARS" ]; then
+  qemu-img create -f raw "$HOME/arch_VARS" 2M
+fi
 
 qemu-system-x86_64 -enable-kvm -cpu host \
-  -drive "$disk1",format=qcow2 \
-  -drive "$disk2",format=qcow2 \
+  -drive file="$disk1",format=qcow2 \
+  -drive file="$disk2",format=qcow2 \
   -cdrom "$ISO" \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.ms.fd \
+  -drive if=pflash,format=raw,file="$HOME"/arch_VARS \
   -m 2G \
   -boot d \
-  -vnc :1 -display none 
+  -monitor stdio \
+  -vnc :2 
