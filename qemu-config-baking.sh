5db77e8e61f5da9bf3883ae2ff02a2321abf2434
Jonathan <koffeinmissbrukarn@gmail.com>
2026-01-29 07:14:45 +0100

    script successfully boots UEFI

--- qemu-config-baking.sh
+++ qemu-config-baking.sh
@@ -3,6 +3,12 @@ ISO=$1
 disk1="$PWD/ArchDisk1.qcow2" # 30 gb TODO: Fix sizes on row 16-17
 disk2="$PWD/ArchDisk2.qcow2" # 30 gb TODO: Fix sizes on row 16-17
 
+READONLY_CODE="/usr/share/OVMF/OVMF_CODE_4M.ms.fd"
+VARS_TEMPLATE="/usr/share/OVMF/OVMF_VARS_4M.fd"
+
+VARS_DIR="$HOME/arch"
+VARS_FILE="$VARS_DIR/arch_VARS_4M.fd"
+
 case "$ISO" in
   *.iso) ;;
   *)
@@ -12,22 +18,31 @@ case "$ISO" in
 esac
 
 # Checking if there disk1 and disk2 is already on the system, if it already is it will skip this step.
-if [ -n "$disk1" ] && [ -n "$disk2" ] && [ ! -e "$disk1" ] && [ ! -e "$disk2" ]; then
-  qemu-img create -f qcow2 "$disk1" 3G
-  qemu-img create -f qcow2 "$disk2" 3G
-fi
 
-if [ ! -f "$HOME/arch_VARS" ]; then
-  qemu-img create -f raw "$HOME/arch_VARS" 2M
+ [ -e "$disk1" ] || qemu-img create -f qcow2 "$disk1" 3G
+ [ -e "$disk2" ] || qemu-img create -f qcow2 "$disk2" 3G
+
+
+mkdir -p "$VARS_DIR"
+
+
+if [ ! -f "$VARS_FILE" ]; then
+  if [ -f "$VARS_TEMPLATE" ]; then
+    cp "$VARS_TEMPLATE" "$VARS_FILE"
+  else 
+  echo "Could not find OMVF_VARS.4M.fd on this system" >&2
+  exit 1
+  fi
 fi
 
 qemu-system-x86_64 -enable-kvm -cpu host \
   -drive file="$disk1",format=qcow2 \
   -drive file="$disk2",format=qcow2 \
   -cdrom "$ISO" \
-  -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.ms.fd \
-  -drive if=pflash,format=raw,file="$HOME"/arch_VARS \
+  -drive if=pflash,format=raw,readonly=on,file=$READONLY_CODE \
+  -drive if=pflash,format=raw,file="$VARS_FILE" \
   -m 2G \
   -boot d \
   -monitor stdio \
-  -vnc :2 
+  -machine q35 \
+  -vnc :1 
